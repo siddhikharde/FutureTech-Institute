99a6a365c09439f5f22add5f77231723ae6818c9
siddhikharde <siddhikharde2005@gmail.com>
2026-01-17 19:59:39 +0530

    Routing done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,26 @@
 
 import React from 'react'
-
+import {Routes, BrowserRouter, Route} from 'react-router'
+import Login from './views/Login'
+import Register from './views/Register'
+import Contact from './views/Contact'
+import About from './views/About'
+import Courses from './views/Courses'
+import Home from './views/Home'
 function App() {
   return (
     <div>
+      <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+         <Route path='/login' element={<Login/>}/>
+          <Route path='/register' element={<Register/>}/>
+           <Route path='/contact' element={<Contact/>}/>
+            <Route path='/about' element={<About/>}/>
+             <Route path='/courses' element={<Courses/>}/>
+
+      </Routes>
+      </BrowserRouter>
       
     </div>
   )
--- client/src/views/Courses.jsx
+++ client/src/views/Courses.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Courses() {
   return (
     <div>
-      
+      Courses
     </div>
   )
 }
