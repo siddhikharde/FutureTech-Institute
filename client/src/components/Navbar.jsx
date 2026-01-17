0c3601844df0efd0a7305ff7489c614cf529d5e8
siddhikharde <siddhikharde2005@gmail.com>
2026-01-17 21:45:44 +0530

    Hamburgur menu created

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,9 +1,12 @@
 import { Link } from "react-router";
 import Button from "./Button";
+import { Menu, X } from "lucide-react";
+import { useState } from "react";
 
 function Navbar(){
+    const [open, setOpen] = useState(false);
   return (
-    <nav className="bg-[#0F172A] shadow-md">
+    <div className="bg-[#0F172A] shadow-md">
       <div className="max-w-7xl mx-auto px-4">
         <div className="flex justify-between items-center h-16">
 
@@ -38,10 +41,33 @@ function Navbar(){
               <Button variant="primary" size="sm" title={" Register"}/>
             </Link>
           </div>
+           <button
+            onClick={() => setOpen(!open)}
+            className="md:hidden text-gray-200 cursor-pointer"
+          >
+            {open ? <X size={26} /> : <Menu size={26} />}
+          </button>
 
         </div>
       </div>
-    </nav>
+      {open && (
+        <div className="md:hidden bg-[#020617] px-4 py-4 space-y-4">
+          <Link onClick={() => setOpen(false)} to="/" className="block text-gray-200">Home</Link>
+          <Link onClick={() => setOpen(false)} to="/courses" className="block text-gray-200">Courses</Link>
+          <Link onClick={() => setOpen(false)} to="/about" className="block text-gray-200">About</Link>
+          <Link onClick={() => setOpen(false)} to="/contact" className="block text-gray-200">Contact</Link>
+
+          <div className=" flex gap-3 items-start justify-start pt-3 border-t border-gray-700">
+            <Link to="/login">
+              <Button variant="outline" size="sm" title={"Login"} className="w-full"/>
+            </Link>
+            <Link to="/register">
+              <Button size="sm" className="w-full" title={"Register"}/>
+            </Link>
+          </div>
+        </div>
+          )}
+    </div>
   );
 };
 
