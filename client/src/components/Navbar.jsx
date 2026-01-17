d9540ce9eb6bd6837ea8490515db687cbfc5d4b4
siddhikharde <siddhikharde2005@gmail.com>
2026-01-17 22:14:58 +0530

    hero section created and basic changes added in navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -6,7 +6,7 @@ import { useState } from "react";
 function Navbar(){
     const [open, setOpen] = useState(false);
   return (
-    <div className="bg-[#0F172A] shadow-md">
+    <div className="bg-[#0F172A] shadow-md sticky top-0 left-0 right-0">
       <div className="max-w-7xl mx-auto px-4">
         <div className="flex justify-between items-center h-16">
 
@@ -18,27 +18,27 @@ function Navbar(){
           </Link>
 
           <div className="hidden md:flex space-x-6">
-            <Link to="/" className="text-gray-200 hover:text-[#06B6D4]">
+            <Link to="/" className="text-gray-200 text-[17px] font-bold hover:text-[#06B6D4]">
               Home
             </Link>
-            <Link to="/courses" className="text-gray-200 hover:text-[#06B6D4]">
+            <Link to="/courses" className="text-gray-200 text-[17px] font-bold hover:text-[#06B6D4]">
               Courses
             </Link>
-            <Link to="/about" className="text-gray-200 hover:text-[#06B6D4]">
+            <Link to="/about" className="text-gray-200 font-bold text-[17px] hover:text-[#06B6D4]">
               About
             </Link>
-            <Link to="/contact" className="text-gray-200 hover:text-[#06B6D4]">
+            <Link to="/contact" className="text-gray-200 font-bold text-[17px] hover:text-[#06B6D4]">
               Contact
             </Link>
           </div>
 
           <div className="hidden md:flex items-center space-x-3">
             <Link to="/login">
-              <Button variant="outline" size="sm" title={"Login"}/>
+              <Button variant="outline" size="md" title={"Login"}/>
             </Link>
 
             <Link to="/register">
-              <Button variant="primary" size="sm" title={" Register"}/>
+              <Button variant="primary" size="md" title={" Register"}/>
             </Link>
           </div>
            <button
@@ -51,18 +51,18 @@ function Navbar(){
         </div>
       </div>
       {open && (
-        <div className="md:hidden bg-[#020617] px-4 py-4 space-y-4">
-          <Link onClick={() => setOpen(false)} to="/" className="block text-gray-200">Home</Link>
-          <Link onClick={() => setOpen(false)} to="/courses" className="block text-gray-200">Courses</Link>
-          <Link onClick={() => setOpen(false)} to="/about" className="block text-gray-200">About</Link>
-          <Link onClick={() => setOpen(false)} to="/contact" className="block text-gray-200">Contact</Link>
+        <div className="md:hidden flex flex-col gap-6 justify-center items-start bg-[#020617] px-4 py-4 fixed right-0 left-0">
+          <Link onClick={() => setOpen(false)} to="/" className="block text-gray-200 text-[17px] font-bold">Home</Link>
+          <Link onClick={() => setOpen(false)} to="/courses" className="block text-gray-200 text-[17px] font-bold">Courses</Link>
+          <Link onClick={() => setOpen(false)} to="/about" className="block text-gray-200 text-[17px] font-bold">About</Link>
+          <Link onClick={() => setOpen(false)} to="/contact" className="block text-gray-200 text-[17px] font-bold">Contact</Link>
 
-          <div className=" flex gap-3 items-start justify-start pt-3 border-t border-gray-700">
+          <div className=" flex gap-3 py-2 w-full items-start justify-start pt-3 border-t border-gray-700">
             <Link to="/login">
-              <Button variant="outline" size="sm" title={"Login"} className="w-full"/>
+              <Button variant="outline" size="md" title={"Login"} className="w-full"/>
             </Link>
             <Link to="/register">
-              <Button size="sm" className="w-full" title={"Register"}/>
+              <Button size="md" className="w-full" title={"Register"}/>
             </Link>
           </div>
         </div>
--- client/src/views/Home.jsx
+++ client/src/views/Home.jsx
@@ -1,13 +1,34 @@
-import React from 'react'
-import Navbar from '../components/Navbar'
+import Button from "../components/Button";
+import Navbar from "../components/Navbar";
+import { useNavigate } from "react-router";
 
-function Home() {
+function Home (){
+  const navigate=useNavigate();
   return (
-    <div>
-      <Navbar/>
-      Home
-    </div>
-  )
-}
+    <div className="bg-[#F8FAFC]">
 
-export default Home
+      <Navbar/>
+      <div className="bg-gradient-to-r from-[#0F172A] via-[#143a8a] to-[#0F172A] text-white">
+        <div className="max-w-7xl mx-auto px-5 py-24 text-center">
+          <h1 className="text-4xl md:text-6xl font-bold mb-6">
+            Learn Skills for the Future with <span className="text-[#38BDF8]">FutureTech</span>
+          </h1>
+          <p className="text-xl text-gray-300 mb-8">
+            Industry-ready courses, expert trainers, and recorded lectures.
+          </p>
+          <div className="flex justify-center gap-4">
+            <Button size="lg"title={"Explore Courses"} onClick={()=>{
+                navigate("/courses")
+            }} />
+            <Button variant="secondary" size="lg" title={"Contact Us"}
+            onClick={()=>navigate("/contact")}/>
+
+          </div>
+        </div>
+      </div>
+
+    </div>
+  );
+};
+
+export default Home;
