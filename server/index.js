bc4e7e4939966f6f9b9162edf13eda461e38ea9b
siddhikharde <siddhikharde2005@gmail.com>
2026-01-20 12:31:29 +0530

    database connected

--- server/index.js
+++ server/index.js
@@ -1,7 +1,23 @@
 import express from 'express'
-const PORT=8080;
+import mongoose from 'mongoose';
+import dotenv from 'dotenv'
+import cors from 'cors';
 const app=express();
+dotenv.config();
+app.use(cors());
+app.use(express.json());
+const PORT=process.env.PORT || 5000;
+const connectDb=async ()=>{
+    try{
+      const con=await mongoose.connect(process.env.MONGO_URI);
+      if(con){
+        console.log("Database Connected Successfully..");
+      }
+    }catch(e){
+      console.log("Error in connecting to database ", e);
+    }
+}
 app.listen(PORT,()=>{
     console.log(`Srever is running on a Port:${PORT}`);
-    
+    connectDb();
 })
