9389057d3557d4c1c52eb669e6c1bb22459ead81
siddhikharde <siddhikharde2005@gmail.com>
2026-01-21 00:03:35 +0530

    load Students data function created

--- client/src/components/adminComponenets/StudentTable.jsx
+++ client/src/components/adminComponenets/StudentTable.jsx
@@ -1,30 +1,63 @@
 import axios from "axios";
+import { setDragLock } from "framer-motion";
 import { useEffect, useState } from "react";
 import toast from "react-hot-toast";
 
 function StudentTable() {
+    const [students, setStudesnts] = useState([]);
+    const [course, setCourse] = useState([]);
+
+    const token = localStorage.getItem("JwtToken");
+    const loadStuents = async () => {
+        try {
+            const response = await axios.get("http://localhost:8080/students", {
+                headers: {
+                    Authorization: `Bearer ${token}`
+                }
+            })
+            if (response.data.success) {
+                setStudesnts(response.data.data);
+            }
+        } catch (e) {
+            toast.error("Failed to load courses");
+        }
+    }
+    useEffect(()=>{
+   loadStuents();
+    },[])
+
+    return (
+        <div className="overflow-x-auto bg-white rounded-xl shadow-md p-6">
+            <h2 className="text-xl font-bold mb-4">Students</h2>
+
+            <table className="w-full border border-gray-200">
+                <thead className="bg-gray-100">
+                    <tr>
+                        <th className="p-3 border">Name</th>
+                        <th className="p-3 border">Email</th>
+                        <th className="p-3 border">Enrolled Courses</th>
+                        <th className="p-3 border">Total Fee</th>
+                        <th className="p-3 border">Remaining Fee</th>
+                        <th className="p-3 border">Enroll</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        students.map((s) => {
+                            const remaining = s.fee.total - s.fee.paid;
+                            return(
+                            <tr key={s._id} className="text-center">
+                                <td className="p-2 border">{s.name}</td>
+                                <td className="p-2 border">{s.email}</td>
+                            </tr>
+                         ) })
+                    }
+                </tbody>
 
 
-  return (
-    <div className="overflow-x-auto bg-white rounded-xl shadow-md p-6">
-      <h2 className="text-xl font-bold mb-4">Students</h2>
-
-      <table className="w-full border border-gray-200 ">
-        <thead className="bg-gray-100">
-          <tr>
-            <th className="p-3 border">Name</th>
-            <th className="p-3 border">Email</th>
-            <th className="p-3 border">Enrolled Courses</th>
-            <th className="p-3 border">Total Fee</th>
-            <th className="p-3 border">Remaining Fee</th>
-            <th className="p-3 border">Enroll</th>
-          </tr>
-        </thead>
-
-        
-      </table>
-    </div>
-  );
+            </table>
+        </div>
+    );
 }
 
 export default StudentTable;
