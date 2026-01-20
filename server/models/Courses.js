f9fed802aaa79908f35075176713615234552d8e
siddhikharde <siddhikharde2005@gmail.com>
2026-01-20 13:11:56 +0530

    models created user and course

---
+++ server/models/Courses.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+import {model, Schema} from mongoose;
+const courseSchema=new Schema({
+    title:String,
+    description:String,
+    price:Number,
+},
+{
+    timestamps:true
+})
+
+const Course=model("Courses", courseSchema);
+export default Course;
---
+++ server/models/User.js
@@ -0,0 +1,36 @@
+import {model, Schema} from 'mongoose'
+const userSchema=new Schema({
+    name:{
+        type:String,
+        required:true,
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    password:{
+        type:String,
+        required:true
+    },
+    role:{
+        type:String,
+        enum:["admin", "student"],
+        default:"student",
+    },
+    fee:{
+        total:Number,
+        paid:{
+            type:Number,
+            default:0
+        }
+    },
+       enrolledCourses:[
+        {
+            type:mongoose.Schema.Types.ObjectId, ref:"Course"
+        }
+    ]
+})
+
+const User=model("User", userSchema);
+export default User;
