c2ed700d3ce12df490ebc0d790e9894cc3768c72
siddhikharde <siddhikharde2005@gmail.com>
2026-01-24 00:40:41 +0530

    navbar updated

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,11 +1,20 @@
-import { Link } from "react-router";
+import { Link, useNavigate } from "react-router";
 import Button from "./Button";
 import { Menu, X } from "lucide-react";
 import { useState } from "react";
 import logo from '../assets/logo.png'
 
 function Navbar(){
+  const navigate=useNavigate();
     const [open, setOpen] = useState(false);
+    const token=localStorage.getItem("JwtToken");
+    const handleLogout=()=>{
+      localStorage.removeItem("JwtToken");
+      localStorage.removeItem("UserData");
+      setTimeout(()=>{
+        navigate("/login");
+      },1000)
+    }
   return (
     <div className="bg-white shadow-md z-1000 sticky top-0 left-0 right-0">
       <div className="max-w-7xl mx-auto px-4">
@@ -35,9 +44,13 @@ function Navbar(){
           </div>
 
           <div className="hidden md:flex items-center space-x-3">
-            <Link to="/login">
-              <Button variant="outline" size="md" title={"Login"}/>
-            </Link>
+             {
+              token?(
+                <Button variant="danger" size="md" title={"Logout"} onClick={handleLogout}/>
+              )
+            :(<Link to="/login">
+              <Button variant="outline" size="md" title={"Login"} className="w-full"/>
+            </Link>)}
           </div>
            <button
             onClick={() => setOpen(!open)}
@@ -56,9 +69,13 @@ function Navbar(){
           <Link onClick={() => setOpen(false)} to="/contact" className="block text-gray-200 text-[17px] font-bold">Contact</Link>
 
           <div className=" flex gap-3 py-2 w-full items-start justify-start pt-3 border-t border-gray-700">
-            <Link to="/login">
+            {
+              token?(
+                <Button variant="danger" size="md" title={"Logout"} onClick={handleLogout}/>
+              )
+            :(<Link to="/login">
               <Button variant="outline" size="md" title={"Login"} className="w-full"/>
-            </Link>
+            </Link>)}
           </div>
         </div>
           )}
