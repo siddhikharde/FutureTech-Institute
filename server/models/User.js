b780b202dcabd95f41921a926695f42a965d28dc
siddhikharde <siddhikharde2005@gmail.com>
2026-01-24 19:07:00 +0530

    user model updated for payment history

--- server/models/User.js
+++ server/models/User.js
@@ -30,6 +30,10 @@ const userSchema=new Schema({
             default:0
         }
     },
+    paymentHistory:{
+        amount:Number,
+        date:{type:Date, default:Date.now}
+    },
        enrolledCourses:[
         {
             type:Schema.Types.ObjectId, ref:"Course"
