bd2aaae4dad8d793f0c6c2e644f0c7ed089988e1
Siddhi Kharde <siddhikharde2005@gamil.com>
2026-09-13 20:43:30 +0530

    error resolved

--- server/models/Lectures.js
+++ server/models/Lectures.js
@@ -1,22 +1,27 @@
-import Course from "./Courses.js";
+import { Schema, model } from "mongoose";
 
-const lectureSchema= new Schema({
-    title:{
-        type:String,
-        required:true
+const lectureSchema = new Schema(
+    {
+        title: {
+            type: String,
+            required: true
+        },
+        description: {
+            type: String
+        },
+        videoUrl: {
+            type: String,
+            required: true
+        },
+        course: {
+            type: Schema.Types.ObjectId,
+            ref: "Course",
+            required: true
+        }
     },
-    description:String,
-    videoUrl:{
-        type:String,
-        required:true
-    },
-    course:{
-        type:Schema.Types.ObjectId,
-        ref:"Course",
-        required:true
-    },
-},{
-    timestamps:true,
-})
+    {
+        timestamps: true
+    }
+);
 
 export default model("Lecture", lectureSchema);
